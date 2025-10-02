322bcd005fa1bc1e9e346bd9e29d22b1f81b6451
Ivan Marukhlenko <ivan.marukh@gmail.com>
2025-10-02 22:16:01 +0300

    fix: build.gradle dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.liquibase:liquibase-core")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
 
     runtimeOnly("org.postgresql:postgresql")
