7218794c779c8c213aa5aaaab8db50264a46c51f
Ivan Marukhlenko <ivan.marukh@gmail.com>
2025-11-25 13:52:30 +0300

    fix: set java version in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,18 @@
 plugins {
     id("java")
+    id("java-library")
     id("org.springframework.boot") version "3.2.0"
     id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "org.itmo.secs"
 version = "1.0"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
 
 repositories {
     mavenCentral()
@@ -17,7 +24,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-
+    
     runtimeOnly("org.postgresql:postgresql")
 
     compileOnly("org.projectlombok:lombok")
