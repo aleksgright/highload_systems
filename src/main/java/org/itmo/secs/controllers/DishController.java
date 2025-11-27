f51a938be3b54e25c45235e6385408765df246f4
Ivan Marukhlenko <ivan.marukh@gmail.com>
2025-11-27 21:00:34 +0300

    feat: implements find-logic in Item and Dish Controllers

--- src/main/java/org/itmo/secs/controllers/DishController.java
+++ src/main/java/org/itmo/secs/controllers/DishController.java
@@ -1,18 +1,28 @@
 package org.itmo.secs.controllers;
 
+import java.util.Map;
+import java.net.http.HttpHeaders;
+import java.util.List;
+
 import lombok.AllArgsConstructor;
+
 import org.itmo.secs.model.dto.DishAddItemDto;
 import org.itmo.secs.model.dto.DishCreateDto;
 import org.itmo.secs.model.dto.DishUpdateNameDto;
 import org.itmo.secs.model.entities.Dish;
 import org.itmo.secs.model.entities.Item;
 import org.itmo.secs.services.DishService;
+import org.itmo.secs.services.JsonConvService;
+import org.itmo.secs.utils.conf.PagingConf;
 import org.itmo.secs.utils.exceptions.ItemNotFoundException;
 import org.springframework.core.convert.ConversionException;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @AllArgsConstructor
 @RestController
@@ -20,6 +30,8 @@ import org.springframework.web.bind.annotation.*;
 public class DishController {
     private final DishService dishService;
     private final ConversionService conversionService;
+    private final JsonConvService jsonConvService;
+    private final PagingConf pagingConf;
 
     @PostMapping
     public ResponseEntity<Void> create(@RequestBody DishCreateDto dishCreateDto) {
@@ -35,23 +47,58 @@ public class DishController {
     public ResponseEntity<Void> updateName(@RequestBody DishUpdateNameDto dishUpdateNameDto) {
         try {
             dishService.updateName(conversionService.convert(dishUpdateNameDto, Dish.class));
-        } catch (ItemNotFoundException e) {
-            return ResponseEntity.notFound().build(); // TODO: NE RABOTAET
+        } catch (ConversionException e) {
+            return ResponseEntity.status(500).build();
         }
         return ResponseEntity.ok().build();
     }
 
-    @PutMapping("/addItem")
+    @GetMapping
+    public ResponseEntity<String> find(
+        @RequestParam(required=false) Long id,
+        @RequestParam(name="pnumber", required=false) Integer _pageNumber,
+        @RequestParam(name="psize", required=false) Integer pageSize
+    ) {
+        if (id != null) {
+            return findById(id);
+        }
+
+        Integer pageNumber;
+        if (_pageNumber != null) {
+            pageNumber = _pageNumber;
+        } else {
+            pageNumber = 0;
+        }
+
+        if (pageSize == null) {
+            return findAll(pageNumber, pagingConf.getDefaultPageSize());
+        } else {
+            return findAll(
+                pageNumber, 
+                (pageSize > pagingConf.getMaxPageSize())
+                ? pagingConf.getMaxPageSize()
+                : pageSize
+            );
+        }
+    }
+
+    public ResponseEntity<String> findById(Long id) {
+        Dish dish = dishService.findById(id);
+        return (dish != null)
+        ? ResponseEntity.ok()
+            .header("Content-Type", "application/json")
+            .body(jsonConvService.conv(dish))
+        : new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
+    public ResponseEntity<String> findAll(Integer pageNumber, Integer pageSize) {
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/items")
     public ResponseEntity<Void> addItem(@RequestBody DishAddItemDto dishAddItemDto) {
         dishService.addItem(dishAddItemDto.itemId(), dishAddItemDto.dishId(), dishAddItemDto.count());
         return ResponseEntity.ok().build();
-    }
-
-    @GetMapping("/findById")
-    public ResponseEntity<Dish> findById(@RequestParam long id) {
-        Dish dish = dishService.findById(id);
-        return dish != null ? new ResponseEntity<>(dish, HttpStatus.OK)
-                : new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 
     // @GetMapping("/findByDate")
--- src/main/java/org/itmo/secs/controllers/ItemController.java
+++ src/main/java/org/itmo/secs/controllers/ItemController.java
@@ -1,16 +1,22 @@
 package org.itmo.secs.controllers;
 
 import lombok.AllArgsConstructor;
+
 import org.itmo.secs.model.dto.ItemUpdateDto;
 import org.itmo.secs.model.entities.Item;
 import org.itmo.secs.model.dto.ItemCreateDto;
 import org.itmo.secs.services.ItemService;
+import org.itmo.secs.services.JsonConvService;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.ArrayList;
+
+import org.itmo.secs.model.dto.ItemDto;
+import org.itmo.secs.utils.conf.PagingConf;
 
 @AllArgsConstructor
 @RestController
@@ -18,11 +24,13 @@ import java.util.List;
 public class ItemController {
     private final ConversionService conversionService;
     private final ItemService itemService;
+    private final JsonConvService jsonConvService;
+    private final PagingConf pagingConf;
 
     @PostMapping
     public ResponseEntity<Void> create(@RequestBody ItemCreateDto itemCreateDto) {
         itemService.save(conversionService.convert(itemCreateDto, Item.class));
-        return ResponseEntity.ok().build();
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PutMapping
@@ -32,15 +40,52 @@ public class ItemController {
     }
 
     @GetMapping
-    public ResponseEntity<List<Item>> findAll(int pageNumber) {
-        List<Item> items = itemService.findAll(pageNumber);
-        return ResponseEntity.ok(items);
+    public ResponseEntity<String> find(
+        @RequestParam(required=false) Long id,
+        @RequestParam(name="pnumber", required=false) Integer _pageNumber,
+        @RequestParam(name="psize", required=false) Integer _pageSize,
+        @RequestParam(required=false) String name
+    ) {
+        if (id != null && name == null && _pageNumber == null && _pageSize == null) {
+            return findById(id);
+        } else if (name != null && _pageNumber == null && _pageSize == null) {
+            return findByName(name);
+        } else {
+            Integer pageNumber = (_pageNumber == null) ? 0 : _pageNumber;
+            Integer pageSize = (_pageSize == null) 
+                ? pagingConf.getDefaultPageSize()
+                : (_pageSize > pagingConf.getMaxPageSize())
+                    ? pagingConf.getMaxPageSize()
+                    : _pageSize;
+
+            return findAll(pageNumber, pageSize);
+        }
     }
 
-    // @GetMapping
-    // public ResponseEntity<Item> findByName(String name) {
-    //     Item item = itemService.findByName(name);
-    //     return item != null ? new ResponseEntity<>(item, HttpStatus.OK)
-    //             : new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-    // }
+    public ResponseEntity<String> findAll(Integer pageNumber, Integer pageSize) {
+        List<Item> items = itemService.findAll(pageNumber, pageSize);
+        List<ItemDto> itemsDto = new ArrayList<>();
+        items.forEach((Item it) -> { 
+            itemsDto.add(conversionService.convert(it, ItemDto.class));
+        });
+        return ResponseEntity.ok(jsonConvService.conv(itemsDto));
+    }
+
+    public ResponseEntity<String> findById(Long id) {
+        Item item = itemService.findById(id);
+        return (item == null) 
+            ? ResponseEntity.status(HttpStatus.NOT_FOUND).build()
+            : ResponseEntity.ok(jsonConvService.conv(
+                conversionService.convert(item, ItemDto.class)
+            ));
+    }
+
+    public ResponseEntity<String> findByName(String name) {
+        Item item = itemService.findByName(name);
+        return (item == null) 
+            ? ResponseEntity.status(HttpStatus.NOT_FOUND).build()
+            : ResponseEntity.ok(jsonConvService.conv(
+                conversionService.convert(item, ItemDto.class)
+            ));
+    }
 }
