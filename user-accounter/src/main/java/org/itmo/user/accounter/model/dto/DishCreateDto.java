5acd2fb42ae64cf8e9e3b1e07a95fff235f84482
aleksgright <Alexander2326@yandex.ru>
2026-01-16 11:28:18 +0300

    wip

--- user-accounter/src/main/java/org/itmo/user/accounter/model/dto/DishCreateDto.java
+++
@@ -1,9 +0,0 @@
-package org.itmo.user.accounter.model.dto;
-
-import io.swagger.v3.oas.annotations.media.Schema;
-
-@Schema(name = "Объект для создания нового блюда", description = "Содержит имя создаваемого блюда")
-public record DishCreateDto(
-    @Schema(description = "Имя блюда", type = "string", example = "Гречка по-купечески")
-    String name
-) { }
